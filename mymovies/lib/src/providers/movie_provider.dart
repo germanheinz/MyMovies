e9106d94970883a3036db1b707446a270b61739e
german <heinz.german@gmail.com>
2020-05-05 20:38:50 +0200

    Creating a Stream

--- mymovies/lib/src/providers/movie_provider.dart
+++ mymovies/lib/src/providers/movie_provider.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:mymovies/src/models/movie.dart';
@@ -7,6 +8,22 @@ class MoviesProvider{
     String _apiKey = '81f888823f079b9941a4534f90d59f34';
     String _url = 'api.themoviedb.org';
     String _language = 'es-Es';
+
+    int _popularPage = 0;
+
+    List<Movie> _populars = new List();
+    //tuberia
+    final _popularStreamController = StreamController<List<Movie>>.broadcast();
+    //proceso para introducir peliculas
+    Function(List<Movie>) get popularsSink => _popularStreamController.sink.add;
+    // proceso para escuchar lo que emita
+    Stream<List<Movie>> get popularsStream => _popularStreamController.stream;
+
+
+    void disposeStreams(){
+      _popularStreamController?.close();
+    }
+
 
     Future<List<Movie>> getInTheatres() async{
       final url = Uri.https(_url, '/3/movie/now_playing', {
@@ -21,15 +38,21 @@ class MoviesProvider{
       return movies.items;
     }
     Future<List<Movie>> getPopulars() async{
+      _popularPage++;
       final url = Uri.https(_url, '/3/movie/popular', {
         'api_key' : _apiKey,
-        'language': _language
+        'language': _language,
+        'page': _popularPage.toString()
       });
+     
       final response = await http.get(url);
       final decodedData = json.decode(response.body);
 
       final movies = new Movies.fromJsonList(decodedData['results']);
       // print(movies.items[0].title);
+
+      _populars.addAll(movies.items);
+      popularsSink(_populars);
       return movies.items;
     }
 }
